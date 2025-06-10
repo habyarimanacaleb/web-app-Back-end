5d84c1ec5968b0ad28418801715c400c572cbbf0
habyarimana caleb <callebhabyar55@gmail.com>
2025-06-10 21:57:29 +0200

    fetch users

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,7 +7,7 @@ router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.post('/logout', userCtrl.logout);
 router.get('/dashboard', isAdmin, userCtrl.adminDashboard);
-router.get('users', userCtrl.getAllUsers);
+router.get('/users', userCtrl.getAllUsers);
 router.get('/users/:id', userCtrl.getUserById);
 router.put('/users/:id', userCtrl.updateUser);
 router.delete('/users/:id', userCtrl.deleteUser);
