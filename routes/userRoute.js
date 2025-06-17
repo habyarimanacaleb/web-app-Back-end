6184ff3ad587254a11d0d09c88bd33811ccf12e7
habyarimana caleb <callebhabyar55@gmail.com>
2025-06-17 21:35:41 +0200

    uddate user routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/userController');
-const isAdmin = require('../middleware/IsAdmin');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.post('/logout', userCtrl.logout);
-router.get('/dashboard', isAdmin, userCtrl.adminDashboard);
+router.get('/dashboard', userCtrl.adminDashboard);
 router.get('/users', userCtrl.getAllUsers);
 router.get('/users/:id', userCtrl.getUserById);
 router.put('/users/:id', userCtrl.updateUser);
